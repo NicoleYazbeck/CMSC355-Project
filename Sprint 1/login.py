b7a8e749a1fcca4ae3cea4f095f709c188326d79
NicoleYazbeck <nicoleyazbeck00@gmail.com>
2025-03-23 15:08:05 -0400

    Update login.py

--- Sprint 1/login.py
+++ Sprint 1/login.py
@@ -16,8 +16,8 @@ def authenticate_login(username, password):
     # Iterate through the list of users 
     for user in data.get("auth", {}).get("users", []):
         if user.get("username") == username and user.get("password") == password:
-            return True
-    return False
+            return user.get("role")
+    return None
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -25,14 +25,21 @@ def login():
     username = request_data.get("username")
     password = request_data.get("password")
 
-    if authenticate_login(username, password):
-        return jsonify({"message": "Login successful"}), 200
+    role = authenticate_login(username, password)
+    
+    if role:
+        return jsonify({"message": "Login successful", "role": role}), 200
     else:
         return jsonify({"error": "Invalid username or password"}), 401
 
+
 @app.route('/patient_menu.html')
-def menu_page():
+def patient_menu():
     return render_template('patient_menu.html')
+
+@app.route('/provider_menu.html')
+def provider_menu():
+    return render_template('provider_menu.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
